cb0ce80f9a8c654c4dfed49665d3d6e041296955
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-05-11 09:34:44 -0300

    Comecando o estudo de Rotas

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Seja bem vindo ao meu app!")
+    res.sendFile(__dirname+"/html/index.html");
 });
 
 app.get("/sobre", function(req, res){
-    res.send("Minha página sobre")
+    res.sendFile(__dirname + "/html/sobre.html");
 });
 
 app.get("/blog", function(req, res){
-    res.send("Bem-vindo ao meu blog!")
+    res.send("Bem-vindo ao meu blog!");
 });
 
 app.get("/ola/:nome/:cargo", function(req, res){
-    res.send("Olá, " + req.params.nome + ", você é " + req.params.cargo)
+    res.send("Olá, " + req.params.nome + ", você é " + req.params.cargo);
 });
 
 
